1dd58e57acbd2a3bb37479ab689b05a3611ad79c
koushikwebdev <kou.saha.ami@gmail.com>
2025-11-30 13:42:32 +0530

    refactor: Update comments for clarity in Computer and Dev classes

--- injection-demo/src/main/java/com/main/e_commerce/Computer.java
+++ injection-demo/src/main/java/com/main/e_commerce/Computer.java
@@ -3,4 +3,6 @@ package com.main.e_commerce;
 public interface Computer {
 
     void compile();
+
+    // void run(); // added a run method to demonstrate interface usage
 }
--- injection-demo/src/main/java/com/main/e_commerce/Dev.java
+++ injection-demo/src/main/java/com/main/e_commerce/Dev.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Component;
 @Component // this means spring will create object, it will be available in bean
 public class Dev {
 
-    @Autowired  // field injection: it is connecting Dev to the Laptop, so now we do not need to create object manually
+    @Autowired  // field injection: it is connecting Dev to the Laptop(when only one object is available), so now we do not need to create object manually
     @Qualifier("desktop") // here we are deciding the which machine dev will use between laptop and desktop, but in the laptop class we have used the @Primary, now the @Qualifier will overwrite this
-    private Computer comp;
+
+    private Computer comp; // this is the interface, so we can use any class that implements this interface
 
 //*    Method 2 : by the constructor
 //    public Dev(Laptop laptop){
