2dc01ceb7e539c8f0ce6d730c1835cec3d1617fe
koushikwebdev <kou.saha.ami@gmail.com>
2025-12-01 20:52:30 +0530

    feat: Add VSCode settings for Java null analysis and update comments in Product class

--- simpleWebApp/src/main/java/com/koushik/simpleWebApp/model/Product.java
+++ simpleWebApp/src/main/java/com/koushik/simpleWebApp/model/Product.java
@@ -4,7 +4,7 @@ package com.koushik.simpleWebApp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
+@Data // it will create getter and setter for this class, by the lombok
 @AllArgsConstructor // it will create constructor for this class, by the lombok
 public class Product {
 
